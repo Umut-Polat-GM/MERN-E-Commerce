8a34e2bfda25c11d1a19d3770ebf926e15100fb2
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-01-24 17:40:00 +0300

    implement user signup and login functionality with MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
+    connectDB();
 });
