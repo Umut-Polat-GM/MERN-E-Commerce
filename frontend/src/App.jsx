8db0ae01b1746722c5e1ab4ad54d269a10f797d8
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-01-30 16:29:56 +0300

    replace LoginUpPage with LoginPage component and update routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
-import LoginUpPage from "./pages/LoginUpPage";
+import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/signup" element={<SignUpPage />} />
-                    <Route path="/login" element={<LoginUpPage />} />
+                    <Route path="/login" element={<LoginPage />} />
                 </Routes>
             </div>
         </div>
