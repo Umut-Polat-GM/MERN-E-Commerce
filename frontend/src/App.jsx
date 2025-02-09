ae1bb70e376f4561b12feba5a141bf30997fb038
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2025-02-09 23:37:11 +0300

    add PurchaseSuccess and PurchaseCancel pages; update routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
 import CartPage from "./pages/CartPage";
 import { useCartStore } from "./store/useCartStore";
+import PurchaseSuccessPage from "./pages/PurchaseSuccessPage";
+import PurchaseCancelPage from "./pages/PurchaseCancelPage";
 
 function App() {
     const { user, checkAuth, checkingAuth } = useUserStore();
@@ -49,6 +51,14 @@ function App() {
                     />
                     <Route path="/category/:category" element={<CategoryPage />} />
                     <Route path="/cart" element={user ? <CartPage /> : <Navigate to="/login" />} />
+                    <Route
+                        path="/purchase-success"
+                        element={user ? <PurchaseSuccessPage /> : <Navigate to="/login" />}
+                    />
+                    <Route
+                        path="/purchase-cancel"
+                        element={user ? <PurchaseCancelPage /> : <Navigate to="/login" />}
+                    />
                 </Routes>
             </div>
         </div>
